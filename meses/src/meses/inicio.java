c07e21b32b8b3533e34f299d02d1a9a8f974c108
DavidMunoz18 <145343117+DavidMunoz18@users.noreply.github.com>
2023-11-21 12:19:54 +0100

    no message

---
+++ meses/src/meses/inicio.java
@@ -0,0 +1,20 @@
+package meses;
+
+public class inicio {
+
+	public static void main(String[] args) {
+		
+		int aux =10;
+      for(int i = 0; i<20; i++) {
+    	  
+    	  aux= aux *2;
+    	  
+    	  System.out.println(aux);
+    	  
+    	  
+      }
+        
+
+	}
+
+}
